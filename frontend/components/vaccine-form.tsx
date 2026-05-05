cc6841723fec475b3e60ae2a29ba926b79aa3c7e
Andres Serrano <andresdavidserranopadilla@gmail.com>
2026-05-04 22:58:55 -0500

    conecta frontend con backend en render

--- frontend/components/vaccine-form.tsx
+++ frontend/components/vaccine-form.tsx
@@ -63,7 +63,7 @@ export function VaccineForm() {
         : "Ninguna"
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://visual-ia.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
